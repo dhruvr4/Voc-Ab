248bf4cc4fd36ecfaa83b6e2fb62edbbbb15d114
dhruv <dhruvr4@illinois.edu>
2020-07-22 23:25:21 -0500

    Hi

--- screens/ChallengeResult.js
+++ screens/ChallengeResult.js
@@ -1,46 +1,54 @@
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 import { StackActions } from '@react-navigation/native';
+import IconBack from 'react-native-vector-icons/AntDesign';
 
-
-export default function ChallengeResult({route,navigation}) {
+export default function ChallengeResult({ route, navigation }) {
   let answer = "Time Up"
   const result = JSON.stringify(route.params.answer)
   const correct = JSON.stringify(route.params.correct)
-  if( result === 'true') {
+  const mod = route.params.mode
+  const per = route.params.perwee
+  if (result === 'true') {
     answer = "You are correct"
   }
   if (result === 'false') {
     answer = "You are wrong"
   }
-  const pushAction = StackActions.push('Challenge',{answer:"default"});
+  const pushAction = StackActions.push('Challenge', { answer: mod });
   return (
-      <View>
-    <Text style = {styles.result}> {answer}</Text>
-    <Text style = {styles.correct}> Correct Answer was = {correct}</Text>
-    <Button title="Again" onPress={() => navigation.dispatch(pushAction)} />
+    <View>
+      <IconBack name="back" size={40} onPress={() => navigation.navigate('Home', { mode: mod, perweek: per })} style={styles.back} />
+
+      <Text style={styles.result}> {answer}</Text>
+      <Text style={styles.correct}> Correct Answer was = {correct}</Text>
+      <Button title="Again" onPress={() => navigation.dispatch(pushAction)} />
     </View>
-    );
+  );
 }
-
-
 const styles = StyleSheet.create({
-result:{
+  back: {
+    paddingTop: 25,
+    paddingLeft: 10,
+    color: 'black',
+    left: 10,
+  },
+  result: {
     paddingTop: 100,
     fontWeight: 'bold',
-    fontSize :40,
+    fontSize: 40,
     backgroundColor: 'white',
-  
-},
-correct:{
-  paddingTop: 40,
-  fontWeight: 'bold',
-  fontSize :30,
-  backgroundColor: 'white',
-},
+
+  },
+  correct: {
+    paddingTop: 40,
+    fontWeight: 'bold',
+    fontSize: 30,
+    backgroundColor: 'white',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fafafa',
--- screens/TimeTrial.js
+++ screens/TimeTrial.js
@@ -10,7 +10,13 @@ import GRE2 from './Data/GRE_list_2.json';
 import GRE3 from './Data/GRE_list_3.json';
 import GRE4 from './Data/GRE_list_4.json';
 import GRE5 from './Data/GRE_list_5.json';
+import SAT1 from './Data/SAT_list_1.json';
+import SAT2 from './Data/SAT_list_2.json';
+import SAT3 from './Data/SAT_list_3.json';
+import SAT4 from './Data/SAT_list_4.json';
+import SAT5 from './Data/SAT_list_5.json';
 
+import { StackActions } from '@react-navigation/native';
 const screenWidth = Math.round(Dimensions.get('window').width);
 const screenHeight = Math.round(Dimensions.get('window').height)
 
@@ -29,40 +35,54 @@ export default function TimeTrial({ navigation, route }) {
     hard: [],
     default: []
   }
-  function create_database() {
-    for (var i = 0; i < 262; i++) {
-      add(GRE1.Adjective[i], GRE1.Word[i], "default");
+  function create_database(val) {
+    if (val == "easy") {
+      for (var i = 0; i < Object.values(SAT1.Adjective).length; i++) {
+        add(SAT1.Adjective[i], SAT1.Word[i], "easy");
+      }
+      for (var i = 0; i < Object.values(SAT2.Adjective).length; i++) {
+        add(SAT2.Adjective[i], SAT2.Word[i], "easy");
+      }
+  }
+  if (val == "medium") {
+    for (var i = 0; i < Object.values(SAT3.Adjective).length; i++) {
+      add(SAT3.Adjective[i], SAT3.Word[i], "medium");
     }
-    for (var i = 0; i < 262; i++) {
-      add(GRE2.Adjective[i], GRE2.Word[i], "default");
+    for (var i = 0; i < Object.values(SAT4.Adjective).length; i++) {
+      add(SAT4.Adjective[i], SAT4.Word[i], "medium");
     }
-    for (var i = 0; i < 262; i++) {
-      add(GRE3.Adjective[i], GRE3.Word[i], "default");
+    for (var i = 0; i < Object.values(SAT5.Adjective).length; i++) {
+      add(SAT5.Adjective[i], SAT5.Word[i], "medium");
     }
-    for (var i = 0; i < 262; i++) {
-      add(GRE4.Adjective[i], GRE4.Word[i], "default");
+  }
+  if (val == "hard") {    
+    for (var i = 0; i < Object.values(GRE1.Adjective).length; i++) {
+      add(GRE1.Adjective[i], GRE1.Word[i], "hard");
     }
-    for (var i = 0; i < 262; i++) {
-      add(GRE5.Adjective[i], GRE5.Word[i], "default");
+    for (var i = 0; i < Object.values(GRE2.Adjective).length; i++) {
+      add(GRE2.Adjective[i], GRE2.Word[i], "hard");
     }
-
-
+    for (var i = 0; i < Object.values(GRE3.Adjective).length; i++) {
+      add(GRE3.Adjective[i], GRE3.Word[i], "hard");
+    }
+    for (var i = 0; i < Object.values(GRE4.Adjective).length; i++) {
+      add(GRE4.Adjective[i], GRE4.Word[i], "hard");
+    }
+    for (var i = 0; i < Object.values(GRE5.Adjective).length; i++) {
+      add(GRE5.Adjective[i], GRE5.Word[i], "hard");
+    }
+  }
   }
   function add(a, def, type) {
-
     const toAdd = new Question(a, def);
     datab[type].push(toAdd)
   }
   function load(val) {
     console.log("Load function started")
-    create_database()
-    //console.log(datab)
-
-
+    create_database(val)
     const ques = datab[val][Math.floor(Math.random() * datab[val].length)]
     txt = ques.question
     cor = ques.correctanswer
-
     var ar = [ques.correctanswer]
     while (ar.length < 4) {
       const ques = datab[val][Math.floor(Math.random() * datab[val].length)]
@@ -70,22 +90,11 @@ export default function TimeTrial({ navigation, route }) {
         ar.push(ques.correctanswer)
       }
     }
-
     arr = ar
     shuffle(arr)
-    //console.log(txt)
-    //console.log(cor)
-    //console.log(options)
   }
-
   function answer(answer) {
     let va = (options).indexOf(correct) === answer
-    if (answer == -1) {
-      va = "Time Up"
-      navigat(va)
-    }
-    //console.log(va)
-
     if (va === true) {
       settotscore(totscore + 1)
     }
@@ -95,41 +104,35 @@ export default function TimeTrial({ navigation, route }) {
     nextQuestion()
   }
   function nextQuestion() {
-    val = "default"
+    let val = "hard"
     const ques = datab[val][Math.floor(Math.random() * datab[val].length)]
     txt = ques.question
     cor = ques.correctanswer
-
     var ar = [ques.correctanswer]
     while (ar.length < 4) {
       const ques = datab[val][Math.floor(Math.random() * datab[val].length)]
       if (!(arr.includes(ques.correctanswer))) {
         ar.push(ques.correctanswer)
       }
-      
     }
-
     arr = ar
     shuffle(arr)
-  setText(txt)
-  setCorrect(cor)
-  setOptions(arr)
-        
+    setText(txt)
+    setCorrect(cor)
+    setOptions(arr)
   }
-
-  function navigat(ans) {
+  function navigat() {
+    console.log("Navigat called")
     clearInterval(time)
-    //console.log(totscore)
-    //console.log(totwrong)
-    navigation.navigate("TimeTrialResult", { answer: totscore, correct: totwrong })
+    const pushAction2 = StackActions.push("TimeTrialResult", { answer: totscore, correct: totwrong, mode: result, perwee: perweek });
+    navigation.dispatch(pushAction2)
   }
-
   function tick() {
-    if (timer == 0) {
-      answer(-1)
+    if (timer < 0.1) {
+      navigat()
     }
     else {
-      setTimer(timer - 1)
+      setTimer(tim - (new Date().getMinutes() * 60 + new Date().getSeconds()) + 10)
     }
   }
   React.useEffect(() => {
@@ -138,33 +141,29 @@ export default function TimeTrial({ navigation, route }) {
       clearInterval(time)
     }
   })
-
   function shuffle(array) {
-
     array.sort(() => Math.random() - 0.5);
   }
-  const result = JSON.stringify(route.params.answer)
-  resul = result.substring(1, result.length - 1)
-  //console.log(resul)
+  const result = route.params.answer
+  const perweek = route.params.perweek
   let txt = ''
   let cor = ''
   let arr = []
-  load(resul)
+  load(result)
   const [text, setText] = React.useState(txt)
   const [options, setOptions] = React.useState(arr)
   const [correct, setCorrect] = React.useState(cor)
   const [totscore, settotscore] = React.useState(0)
   const [totwrong, settotwrong] = React.useState([])
+  let [tim, settim] = React.useState(new Date().getMinutes() * 60 + new Date().getSeconds())
   let [timer, setTimer] = React.useState(10)
   let time = null
-  //let time = setInterval(this.tick, 1000);
   return (
     <View style={{ flex: 1, backgroundColor: '#F0FFF0' }}>
       <Text style={styles.timer}> {timer}</Text>
       <View style={styles.QuestionContainer}>
         <Text style={styles.text}> {text}</Text>
       </View>
-
       <View style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: '#F0FFF0' }}>
         <TouchableOpacity style={styles.AnswerButtonBlack} onPress={() => { answer(0) }}>
           <Text style={styles.AnswerText}>{options[0]}</Text>
