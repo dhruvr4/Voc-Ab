a596797876352ddfffc81f1569d4a5143be7d1bc
shashanksinghania <39347320+shashanksinghania@users.noreply.github.com>
2020-09-07 15:15:45 +0530

    Result page made dynamic

--- src/screens/ChallengeResult.js
+++ src/screens/ChallengeResult.js
@@ -4,6 +4,7 @@ import { StackActions } from '@react-navigation/native';
 import IconBack from 'react-native-vector-icons/AntDesign';
 import IconForward from 'react-native-vector-icons/SimpleLineIcons'
 import firebase from 'firebase'
+import {normalize} from '../util'
 
 const screenWidth = Math.round(Dimensions.get('window').width);
 const screenHeight = Math.round(Dimensions.get('window').height);
@@ -67,35 +68,35 @@ export default function ChallengeResult({ route, navigation }) {
   })
   const pushAction = StackActions.push('Challenge', { answer: mod, lvl: lvl, xp: xp, pu: pu, words_done: words_done });
   return (
-    <View style={{ flex: 1, backgroundColor: 'white' }}>
-      <IconBack name="home" size={40} onPress={() => navigation.navigate('Home', { mode: mod, lvl: lvl, xp: xp, pu: pu, words_done: words_done })} style={styles.home} />
+    <View style={{ flex: 1, backgroundColor: '#f5fcfc' }}>
+      <IconBack name="home" size={normalize(40)} onPress={() => navigation.navigate('Home', { mode: mod, lvl: lvl, xp: xp, pu: pu, words_done: words_done })} style={styles.home} />
 
       <Text style={{
-        paddingTop: screenHeight / 20,
-        fontWeight: '700',
-        fontSize: 48,
+        paddingTop: '4%',
+        fontSize: normalize(48),
+        fontFamily: 'ReemKufi',
         alignSelf: 'center',
         color: Color,
-      }}> {answer}</Text>
-      <Text style={{ fontSize: 40, alignSelf: 'center', paddingTop: screenHeight / 10, }}>The answer is </Text>
+      }}>{answer}</Text>
+      <Text style={{ fontSize: normalize(40), alignSelf: 'center', paddingTop: '11%', fontFamily: 'ReemKufi'}}>The answer is:</Text>
       <View style={styles.WordOfDay}>
         <Text style={styles.WordText}>{correct.charAt(0).toUpperCase() + correct.substring(1, correct.length)}</Text>
         <Text style={styles.DefinitionText}>{question.charAt(0).toUpperCase() + question.substring(1, question.length)}</Text>
       </View>
-      <IconForward name="control-forward" size={50} onPress={() => navigation.dispatch(pushAction)} style={styles.forward} />
+      <IconForward name="control-forward" size={normalize(50)} onPress={() => navigation.dispatch(pushAction)} style={styles.forward} />
     </View>
   );
 }
 const styles = StyleSheet.create({
   forward: {
     alignSelf: 'center',
-    paddingTop: screenHeight / 10
+    marginTop: '20%'
   },
   correct: {
     paddingTop: 40,
-    fontWeight: 'bold',
     fontSize: 30,
     backgroundColor: 'white',
+    fontFamily: 'ReemKufi',
   },
   container: {
     flex: 1,
@@ -130,18 +131,20 @@ const styles = StyleSheet.create({
     left: 10,
   },
   WordText: {
-    fontWeight: '700',
-    fontSize: 32,
+    fontFamily: 'ReemKufi',
+    fontSize: normalize(34),
     alignSelf: 'center',
-    paddingLeft: screenWidth / 30,
-    paddingTop: screenHeight / 60
+    paddingTop: normalize(10),
+    fontWeight:'400',
   },
   DefinitionText: {
     fontWeight: '300',
-    fontSize: 24,
-    paddingLeft: screenWidth / 10,
+    fontFamily: 'ReemKufi',
+    fontSize: normalize(24),
+    paddingLeft: normalize(25),
     paddingRight: screenWidth / 35,
-    paddingTop: screenHeight / 20
+    paddingTop: normalize(25),
+    marginBottom: '11%'
   },
   WordOfDay: {
     alignItems: 'flex-start',
@@ -149,7 +152,6 @@ const styles = StyleSheet.create({
     marginTop: screenHeight / 20,
     backgroundColor: 'white',
     width: screenWidth - 40,
-    height: screenHeight / 4,
     alignSelf: 'center',
     shadowColor: "#000",
     shadowOffset: {
