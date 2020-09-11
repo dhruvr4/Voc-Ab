1aa4539ce213d52da02d07b93d87ddfbe9b8ee04
shashanksinghania <39347320+shashanksinghania@users.noreply.github.com>
2020-09-12 04:22:47 +0530

    Leadrboard UI update

--- src/screens/Leaderboard.js
+++ src/screens/Leaderboard.js
@@ -2,20 +2,17 @@ import * as React from 'react';
 import { StyleSheet, Text, View, FlatList, Dimensions,ActivityIndicator } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { StackActions } from '@react-navigation/native';
-import IconBack from 'react-native-vector-icons/AntDesign';
 import IconForward from 'react-native-vector-icons/SimpleLineIcons';
 import firebase from 'firebase'
 import { normalize } from '../util';
 import datab from './WordsDatabase';
+import IconBack from 'react-native-vector-icons/EvilIcons';
+
 
 
 const screenWidth = Math.round(Dimensions.get('window').width);
 const screenHeight = Math.round(Dimensions.get('window').height);
 
-let count = -36;
-function rank() { 
-  return count ++;
-}
 
 export default function Leaderboard({ route, navigation }) {
     let unsubscribe
@@ -24,37 +21,40 @@ export default function Leaderboard({ route, navigation }) {
         componentDidMount() {
             var user = firebase.auth().currentUser;
             var db = firebase.firestore();
-               var groupsRef = db.collection("Users");
-                unsubscribe = groupsRef
-                    .onSnapshot(function namae(querySnapshot) {
-                        var cities = [];
-                        querySnapshot.forEach(function (doc) {
-                                
-                                var color2=doc.data().fullName==user.displayName?"#D3D3D3":"white"
-                                cities.push({ name: doc.data().fullName, level: doc.data().level,color:color2});
-                        });
-                        cities.sort((a, b) => (a.level < b.level) ? 1 : -1)
-                        //console.log(cities)
-                         this.setState({ groupIDs: cities,loading:false});
-                    }.bind(this));
+            var groupsRef = db.collection("Users");
+            unsubscribe = groupsRef
+                          .onSnapshot(function namae(querySnapshot) {
+                            var cities = [];
+                            querySnapshot.forEach(function (doc) {
+                              var color2=doc.data().fullName==user.displayName?"#D3D3D3":"white"
+                              cities.push({ name: doc.data().fullName, level: doc.data().level,color:color2});
+                            });
+                            cities.sort((a, b) => (a.level < b.level) ? 1 : -1)
+                            //console.log(cities)
+                            this.setState({ groupIDs: cities,loading:false});
+                          }.bind(this));
         }
+
+
         componentWillMount() {
             return unsubscribe;
         }
+
+        
         render() {
-            const renderItem = ({ item }) => (
-                <View style={{flexDirection : 'row' ,flex : 1, padding: 3, 
-                marginTop : normalize(10),marginLeft : normalize(10), marginRight : normalize(10), 
+            const renderItem = ({ item, index }) => (
+                <View style={{flexDirection : 'row' ,flex : 1, paddingVertical: normalize(5), 
+                marginTop : normalize(4), marginHorizontal: '4%', 
                 }}>
-                        <View style = {{flex : 0.5, alignItems : 'center', borderRadius : 10, borderWidth: 1, borderColor: 'grey', 
-                        marginRight : normalize(20), backgroundColor:item.color}}>
-                          <Text style={styles.connectOptionsText}>{rank()}</Text>
+                        <View style = {{flex : 0.6, alignItems : 'center', borderTopLeftRadius: 15, borderBottomLeftRadius: 15, borderTopRightRadius:8, borderBottomRightRadius:8, 
+                        borderWidth: 1.3, borderColor: 'grey', marginRight : '1%', backgroundColor:item.color}}>
+                          <Text style={styles.connectOptionsText}>{'#'+(index + 1)}</Text>
                         </View>
-                        <View style = {{flex : 2.5, backgroundColor:item.color, borderRadius : 10, borderWidth: 1, borderColor: 'grey', paddingLeft : 9,
+                        <View style = {{flex : 2.3, backgroundColor:item.color, borderRadius : 8, borderWidth: 1.3, borderColor: 'grey', paddingLeft : 3,
                         alignItems : 'center'}}>
                           <Text style={styles.connectOptionsText}>{item.name}</Text>
                         </View>
-                        <View style = {{flex : 0.5, alignItems : 'center', borderRadius : 10, borderWidth: 1, borderColor: 'grey', marginLeft : normalize(20), backgroundColor:item.color}}>
+                        <View style = {{flex : 0.6, alignItems : 'center', borderRadius : 8, borderTopRightRadius:15, borderBottomRightRadius:15, borderWidth: 1.3, borderColor: 'grey', marginLeft : '1%', backgroundColor:item.color}}>
                           <Text style={styles.connectOptionsText}>{item.level}</Text>
                         </View>   
                 </View>
@@ -68,6 +68,7 @@ export default function Leaderboard({ route, navigation }) {
                             </View>
                         ) : null
                     }
+                    
                     <FlatList
                         data={this.state.groupIDs}
                         renderItem={renderItem}
@@ -76,7 +77,7 @@ export default function Leaderboard({ route, navigation }) {
                             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', marginVertical: 20 }}>
                                 {
                                     this.state.loading ? null : (
-                                        <Text style={{ fontSize: 15, fontFamily : 'ReemKufi' }} >No such Group found... try something else</Text>
+                                        <Text style={{ fontSize: 15, fontFamily : 'ReemKufi' }} >Unable to load... try something else</Text>
                                     )
                                 }
                             </View>
@@ -86,18 +87,21 @@ export default function Leaderboard({ route, navigation }) {
         }
     }
     return (
-    <View style={{ flex: 1, backgroundColor: 'white' }}>         
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <View style={styles.head}>
+                <Text style={styles.heading}>Leaderboard</Text>
+                <IconBack name="arrow-left" size={normalize(55)} onPress={() => navigation.navigate('Home', {mode: route.params.mode,lvl:route.params.lvl,xp:route.params.xp,pu:route.params.pu,words_done:route.params.words_done})} style={styles.back} />
+            </View>
       <ScrollView>
-      <IconBack name="home" size={normalize(35)} onPress={() => navigation.navigate('Home', { mode: route.params.mode, lvl: route.params.lvl, xp: route.params.xp, pu: route.params.pu, words_done: route.params.words_done })} style={styles.home} />
       <View style={{flexDirection : 'row' ,flex : 1, padding: 3, 
       marginTop : normalize(10),marginLeft : normalize(10), marginRight : normalize(10),}}>
-        <View style = {{flex : 0.5, alignItems : 'flex-start', borderRadius : 10, marginRight : normalize(10),}}>
+        <View style = {{flex : 0.5, alignItems : 'flex-start', borderRadius : 10, marginLeft : '2.5%',}}>
          <Text style={styles.titleText}>Rank</Text>
         </View>
-        <View style = {{flex : 1, borderRadius : 10, alignItems : 'center'}}>
+        <View style = {{flex : 1.1, borderRadius : 10, alignItems : 'center'}}>
           <Text style={styles.titleText}>Name</Text>
         </View>
-        <View style = {{flex : 0.5, alignItems : 'center', marginLeft : normalize(20)}}>
+        <View style = {{flex : 0.4, alignItems : 'center', marginRight : '0.5%'}}>
           <Text style={styles.titleText}>Level</Text>
         </View>        
       </View>
@@ -108,74 +112,27 @@ export default function Leaderboard({ route, navigation }) {
   );
 }
 const styles = StyleSheet.create({
-  home: {
-    paddingTop: screenHeight / 25,
-    paddingLeft: 10,
-    color: 'black',
-    left: 10,
-  },
+  
   titleText : {
     color : 'black',
     fontFamily : 'ReemKufi',
-    fontSize : normalize(40)
+    fontSize : normalize(30)
   },
+  heading: {
+    fontSize: normalize(48),
+    paddingLeft: 15,
+    fontFamily: 'ReemKufi',
+},
+head: {
+  width: '95%',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingTop: normalize(55),
+  flexDirection: 'row'
+},
   connectOptionsText:{
     color:'black',
     fontFamily : 'ReemKufi',
-    fontSize : normalize(34),
-  },
-  result: {
-    paddingTop: screenHeight / 35,
-    fontSize: normalize(40),
-    fontWeight: '300',
-    alignSelf: 'center',
-    fontFamily: 'ReemKufi',
-  },
-  correct: {
-    paddingTop: 40,
-    fontWeight: 'bold',
-    fontSize: 30,
-    backgroundColor: 'white',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fafafa',
-  },
-  contentContainer: {
-    paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
-  },
-  WordText: {
-    fontFamily: 'ReemKufi',
-    fontSize: normalize(32),
-    alignSelf: 'center',
-    paddingTop: '3%',
-    color: 'white'
-  },
-  DefinitionText: {
-    fontFamily: 'ReemKufi',
-    fontSize: normalize(24),
-    paddingHorizontal: '7%',
-    paddingTop: '4%',
-    alignSelf: 'center',
-    color: 'white'
+    fontSize : normalize(27),
   },
 });
